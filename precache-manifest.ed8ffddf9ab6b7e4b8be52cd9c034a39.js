700fb723ded47a72811e6d6422806460dc99eb38
KeerthikaChandra <keerthikachandrashekhar0410@gmail.com>
2019-12-02 15:31:24 +0530

    Updates

---
+++ precache-manifest.ed8ffddf9ab6b7e4b8be52cd9c034a39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca94e07bc9920919fa5a7a763386f7fd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "28bf91726f25805d8abc",
+    "url": "/monsters-rolodex/static/css/main.e6571b35.chunk.css"
+  },
+  {
+    "revision": "5b66571292b2cce7e259",
+    "url": "/monsters-rolodex/static/js/2.73b508e1.chunk.js"
+  },
+  {
+    "revision": "28bf91726f25805d8abc",
+    "url": "/monsters-rolodex/static/js/main.df7ed797.chunk.js"
+  },
+  {
+    "revision": "101eb8996a18ef80df29",
+    "url": "/monsters-rolodex/static/js/runtime-main.0f0d111b.js"
+  }
+]);
